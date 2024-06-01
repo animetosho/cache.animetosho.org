72c5d9aeacfd612108833015edd236d8ef1f1389
animetosho <animetosho@users.noreply.github.com>
2024-06-01 22:53:43 +1000

    Fixes

--- app/nyaasi.js
+++ app/nyaasi.js
@@ -112,7 +112,7 @@ const page_html = (title, body, head, is_sukebei) => {
 		<header>
 			<span id="header_left"><a href="https://animetosho.org/">Anime Tosho</a> &raquo; <a href="/nyaasi${is_sukebei ? '-sukebei':''}/">${site_name}</a></span>
 			<a id="header_right" href="https://github.com/animetosho/cache.animetosho.org">
-				<svg id="gh_icon" viewbox="0 0 98 96" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"/></svg>
+				<svg id="gh_icon" viewbox="0 0 98 96" xmlns="http://www.w3.org/2000/svg" role="img"><title>Github</title><path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"/></svg>
 			</a>
 		</header>
 		<main>${body}</main>
@@ -160,7 +160,7 @@ module.exports = {
 			
 			resp.setHeader('Content-Type', 'text/html; charset=utf-8');
 			resp.setHeader('Cache-Control', 'max-age=7200, public, immutable');
-			resp.setHeader('Content-Security-Policy', 'default-src \'none\'; img-src https:; style-src \'self\'');
+			resp.setHeader('Content-Security-Policy', 'default-src \'none\'; img-src https:; style-src \'self\' \'unsafe-inline\'');
 			resp.setHeader('Referrer-Policy', 'no-referrer');
 			
 			const htmlHead = '<link rel="canonical" href="https://cache.animetosho.org/nyaasi'+if_sukebei('-sukebei')+'/view/'+id+'"/>';
@@ -222,7 +222,7 @@ module.exports = {
 					const ava_buf = Buffer.allocUnsafe(4);
 					ava_buf.writeUint32BE(cmt.ava_stamp);
 					const ava_slug = ava_buf.toString('base64').replace(/[+\/=]/g, m=>({'+':'-','/':'_','=':''}[m[0]]));
-					cmt_ava = `<div class="user_avatar"><img src="${html.escape(getPhotonURL('https://nyaa.si/user/'+encodeURIComponent(cmt.username)+'/avatar-'+ava_slug))}" loading="lazy"/></div>`;
+					cmt_ava = `<div class="user_avatar"><img src="${html.escape(getPhotonURL('https://nyaa.si/user/'+encodeURIComponent(cmt.username)+'/avatar-'+ava_slug))}" loading="lazy" alt=""/></div>`;
 				}
 				return `<tr id="com-${cmt.id}" class="comment_container${extra_class}">
 					<th class="comment_info_wide">${cmt_date}${cmt_user}${cmt_ava}</th>
